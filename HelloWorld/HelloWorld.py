4fa3f3f4cd3bb4e511839c455d9de81e030192c4
tintinkmitl <yougtin@gmail.com>
2015-03-13 14:43:59 +0700

    ....

--- HelloWorld/HelloWorld.py
+++ HelloWorld/HelloWorld.py
@@ -1,1 +1,135 @@
-print('Hello World')
+import sys
+from PySide.QtCore import*
+from PySide.QtGui import*
+
+class Simple_drawing_window(QWidget):
+    def __init__(self):
+        QWidget.__init__(self, None)
+        self.setWindowTitle("Simple Drawing")
+        self.rabbit = QImage("image/rabbit.png")
+
+    def paintEvent(self, e):
+        p = QPainter()
+        p.begin(self)
+
+        p.setPen(QColor(0, 0, 0))
+        p.setBrush(QColor(0, 127, 0))
+        p.drawPolygon([
+            QPoint( 70, 100), QPoint(100, 110),
+            QPoint(130, 100), QPoint(100, 150),
+        ])
+
+        p.setPen(QColor(255, 127, 0))
+        p.setBrush(QColor(255, 127, 0))
+        p.drawPie(50, 150, 100, 100, 0, 180 * 16)
+
+        p.drawPolygon([
+            QPoint(50, 200), QPoint(150, 200), QPoint(100, 400),
+        ])
+
+        p.drawImage(QRect(200, 100, 320, 320), self.rabbit)
+        p.end()
+
+class Simple_drawing_window1(QWidget):
+    def __init__(self):
+        QWidget.__init__(self, None)
+        self.setWindowTitle("Simple Drawing")
+        self.rabbit = QImage("image/rabbit.png")
+
+    def paintEvent(self, e):
+        p = QPainter()
+        p.begin(self)
+
+        p.setPen(QColor(0, 0, 0))
+        p.setBrush(QColor(0, 127, 0))
+        p.drawPolygon([
+            QPoint( 70, 100), QPoint(100, 110),
+            QPoint(130, 100), QPoint(100, 150),
+        ])
+
+        p.setPen(QColor(255, 127, 0))
+        p.setBrush(QColor(250, 50, 0))
+        p.drawPie(50, 150, 100, 100, 0, 180 * 16)
+
+        p.drawPolygon([
+            QPoint(50, 200), QPoint(150, 200), QPoint(100, 400),
+        ])
+
+        p.drawImage(QRect(200, 100, 320, 320), self.rabbit)
+        p.end()
+
+class Simple_drawing_window2(QWidget):
+    def __init__(self):
+        QWidget.__init__(self, None)
+        self.setWindowTitle("Simple Drawing")
+        self.rabbit = QImage("image/rabbit.png")
+
+    def paintEvent(self, e):
+        p = QPainter()
+        p.begin(self)
+
+        p.setPen(QColor(0, 0, 0))
+        p.setBrush(QColor(0, 127, 0))
+        p.drawPolygon([
+            QPoint( 70, 100), QPoint(100, 110),
+            QPoint(130, 100), QPoint(100, 150),
+        ])
+
+        p.setPen(QColor(255, 127, 0))
+        p.setBrush(QColor(0, 150, 0))
+        p.drawPie(50, 150, 100, 100, 0, 180 * 16)
+
+        p.drawPolygon([
+            QPoint(50, 200), QPoint(150, 200), QPoint(100, 400),
+        ])
+
+        p.drawImage(QRect(200, 100, 320, 320), self.rabbit)
+        p.end()
+
+class Simple_drawing_window3(QWidget):
+    def __init__(self):
+        QWidget.__init__(self, None)
+        self.setWindowTitle("Simple Drawing")
+        self.rabbit = QImage("image/rabbit.png")
+
+    def paintEvent(self, e):
+        p = QPainter()
+        p.begin(self)
+
+        p.setPen(QColor(0, 0, 0))
+        p.setBrush(QColor(100, 400, 0))
+        p.drawPolygon([
+            QPoint( 70, 100), QPoint(100, 110),
+            QPoint(130, 100), QPoint(100, 150),
+        ])
+
+        p.setPen(QColor(255, 127, 0))
+        p.setBrush(QColor(110, 450, 0))
+        p.drawPie(50, 150, 100, 100, 0, 180 * 16)
+
+        p.drawPolygon([
+            QPoint(50, 200), QPoint(150, 200), QPoint(100, 400),
+        ])
+
+        p.drawImage(QRect(200, 100, 320, 320), self.rabbit)
+        p.end()
+
+def main():
+    app = QApplication(sys.argv)
+
+    w = Simple_drawing_window()
+    w.show()
+
+    w1 = Simple_drawing_window1()
+    w1.show()
+
+    w2 = Simple_drawing_window2()
+    w2.show()
+
+    w3 = Simple_drawing_window3()
+    w3.show()
+
+    return app.exec_()
+
+if __name__ == "__main__":
+    sys.exit(main())
